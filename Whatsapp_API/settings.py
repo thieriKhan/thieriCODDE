de92355215e14ffffd21957089880694cf055075
Thieri Khan <55806139+thieriKhan@users.noreply.github.com>
2020-12-02 23:33:10 +0100

    Update settings.py

--- Whatsapp_API/settings.py
+++ Whatsapp_API/settings.py
@@ -126,7 +126,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT  =os.path.join(BASE_DIR,'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 STATICFILES_DIRS = [
